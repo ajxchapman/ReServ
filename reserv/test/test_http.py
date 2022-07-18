57479cddb12726b96cbd97a3c317ce7ffc5bcd92
Alex Chapman <3729326+ajxchapman@users.noreply.github.com>
2022-07-18 18:10:08 +0000

    More HTTP tests

--- reserv/test/test_http.py
+++ reserv/test/test_http.py
@@ -12,6 +12,7 @@ class HttpRequest(DummyRequest):
     def __init__(self, parsed_uri, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
+        self.startedWriting = False
         self._serverName = parsed_uri.hostname.encode()
         self.uri = parsed_uri.path
         if len(parsed_uri.query):
@@ -20,6 +21,13 @@ class HttpRequest(DummyRequest):
 
     def isSecure(self):
         return self.uri.startswith(b'https')
+
+    def write(self, data):
+        super().write(data)
+        self.startedWriting = True
+
+    def getResponseHeader(self, name):
+        return self.responseHeaders.getRawHeaders(name.lower(), [None])[0]
 
 class TestHTTP(unittest.TestCase):
     def setUp(self) -> None:
@@ -30,18 +38,24 @@ class TestHTTP(unittest.TestCase):
         parsed_uri = urlparse(uri)
         req = HttpRequest(parsed_uri, [])
         child = self.factory.resource.getChild(parsed_uri.path[1:], req)
-        body = child.render(req)
+        req.write(child.render(req))
 
-        return (req, body, child)
+        return (req, b''.join(req.written), child)
 
     def test_404(self):
         (req, _, _) = self.make_request("http://example.com/DoesNotExist")
         
         self.assertEqual(req.responseCode, 404)
 
-    def test_file(self):
-        (req, body, _) = self.make_request("http://example.com/index.html")
+    def test_serve(self):
+        (req, body, _) = self.make_request("http://example.com/serve")
         
         self.assertEqual(req.responseCode, 200)
         self.assertEqual(body, b'Hello World')
         
+    def test_raw(self):
+        (req, body, _) = self.make_request("http://example.com/raw")
+
+        self.assertEqual(req.responseCode, 202)
+        self.assertEqual(req.getResponseHeader("X-Raw"), "Raw Header")
+        self.assertEqual(body, b'Raw Body')
